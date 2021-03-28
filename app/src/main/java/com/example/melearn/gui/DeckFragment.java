7f158e4e35aa78a3e256b0b9755e1e6024967506
MarkAntonioGiovani <markantoniogiovani@gmail.com>
2021-03-28 04:14:48 +0200

    Login functionality

--- app/src/main/java/com/example/melearn/gui/DeckFragment.java
+++ app/src/main/java/com/example/melearn/gui/DeckFragment.java
@@ -4,17 +4,43 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.melearn.R;
+import com.example.melearn.MainActivity;
+import com.example.melearn.logic.Deck;
 
 public class DeckFragment extends Fragment {
+    MainActivity.user.getDecks().get()
+    private String name;
+    private Deck deck;
+    private TextView txtVwHeader;
+    private Button btnAddCard;
+
+    public DeckFragment(Deck deck) {
+        this.name = deck.getName();
+        this.deck = deck;
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_deck, container, false);
+        View view = inflater.inflate(R.layout.fragment_deck, container, false);
+        txtVwHeader = (TextView) view.findViewById(R.id.txtVwHeader);
+        txtVwHeader.setText(this.name);
+        btnAddCard = (Button) view.findViewById(R.id.btnAdd);
+        btnAddCard.setOnClickListener(createCard(view));
+        return view;
+    }
+
+    private View.OnClickListener createCard(View view) {
+
+        return null;
     }
 }
